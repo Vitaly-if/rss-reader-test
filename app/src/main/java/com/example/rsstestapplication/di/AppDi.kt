a02992deacdd642eba117d73a33b083c19d35014
Vitaly <>
2022-06-02 17:34:23 +0700

    add Koin

---
+++ app/src/main/java/com/example/rsstestapplication/di/AppDi.kt
@@ -0,0 +1,4 @@
+package com.example.rsstestapplication.di
+
+class AppDi {
+}
---
+++ app/src/main/java/com/example/rsstestapplication/di/RepositoryDi.kt
@@ -0,0 +1,13 @@
+package com.example.rsstestapplication.di
+
+import org.koin.dsl.module
+
+val repositoryModule = module {
+
+//    single<DataBase> {
+//        getDatabase(context = get())
+//}
+//    single<Repository> {
+//        Repository(Database = get())
+//    }
+}
